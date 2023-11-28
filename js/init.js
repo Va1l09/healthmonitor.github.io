384390cefd6f6d27f53eb205b4b188976a91d920
Va1l09 <150830353+Va1l09@users.noreply.github.com>
2023-11-28 17:55:21 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,8 +1,17 @@
-const firebaseConfig = {
+/*  Inicializa Firebase con la
+ * configuración del proyecto.
+ * Revisa la configuración en tu
+ * servidor de Firebase y cópiala
+ * aquí sustituyendo los
+ * asteriscos. Los campos deben
+ * quedar igual que en tu
+ * servidor. */
+// @ts-ignore
+firebase.initializeApp({
     apiKey: "AIzaSyAX0WWDug1NKUtD7Xu2H5SnBqPRImsqjSA",
     authDomain: "healthmonitor-1f629.firebaseapp.com",
     projectId: "healthmonitor-1f629",
     storageBucket: "healthmonitor-1f629.appspot.com",
     messagingSenderId: "907503323084",
     appId: "1:907503323084:web:19394f09761e7284717f69"
-  };
+});
