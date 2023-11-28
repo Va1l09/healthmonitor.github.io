3a390406ac4871320a10ef8128492a93f2b787d4
Va1l09 <150830353+Va1l09@users.noreply.github.com>
2023-11-28 17:53:43 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,8 +1,8 @@
-firebase.initializeApp({
+const firebaseConfig = {
     apiKey: "AIzaSyAX0WWDug1NKUtD7Xu2H5SnBqPRImsqjSA",
-  authDomain: "healthmonitor-1f629.firebaseapp.com",
-  projectId: "healthmonitor-1f629",
-  storageBucket: "healthmonitor-1f629.appspot.com",
-  messagingSenderId: "907503323084",
-  appId: "1:907503323084:web:19394f09761e7284717f69"
-});
+    authDomain: "healthmonitor-1f629.firebaseapp.com",
+    projectId: "healthmonitor-1f629",
+    storageBucket: "healthmonitor-1f629.appspot.com",
+    messagingSenderId: "907503323084",
+    appId: "1:907503323084:web:19394f09761e7284717f69"
+  };
